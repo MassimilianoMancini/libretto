5a5e4d305203634787ca6e2ad79d4b59e35e9feb
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2021-09-14 15:31:01 +0200

    working on UI

--- src/main/java/com/example/libretto/view/swing/LibrettoSwingView.java
+++ src/main/java/com/example/libretto/view/swing/LibrettoSwingView.java
@@ -1,106 +1,173 @@
 package com.example.libretto.view.swing;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import com.example.libretto.model.Exam;
 import com.example.libretto.view.LibrettoView;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import javax.swing.JTextField;
-import java.awt.Insets;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JFormattedTextField;
 
 public class LibrettoSwingView extends JFrame implements LibrettoView {
+	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtId;
+	private JTextField txtDescription;
+	private JFormattedTextField txtWeight;
+	private JComboBox<String> cmbGrade;
+	private JFormattedTextField txtDate;
+	
+	private JList<Exam> lstExam;
+	private DefaultListModel<Exam> lstExamModel;
+	
+	
+	private JButton btnSave;
+	private List<JTextField> fieldList = new ArrayList<>();
+	private JScrollPane scrollPane;
+
+	
 	public LibrettoSwingView() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 50, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
+		setTitle("Libretto universitario");
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 500);
+		contentPane = new JPanel();
+		contentPane.setMinimumSize(new Dimension(7, 20));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
 		
-		JLabel lblId = new JLabel("Id");
-		lblId.setName("lblId");
-		GridBagConstraints gbcLblId = new GridBagConstraints();
-		gbcLblId.insets = new Insets(0, 0, 5, 5);
-		gbcLblId.anchor = GridBagConstraints.EAST;
-		gbcLblId.gridx = 0;
-		gbcLblId.gridy = 0;
-		getContentPane().add(lblId, gbcLblId);
+	gbl_contentPane.columnWidths = new int[] {30, 50, 30, 50, 30, 50, 30, 50, 50};
+		gbl_contentPane.rowHeights = new int[]{354, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
 		
-		txtId = new JTextField();
-		txtId.setName("txtId");
-		GridBagConstraints gbcTxtId = new GridBagConstraints();
-		gbcTxtId.anchor = GridBagConstraints.WEST;
-		gbcTxtId.insets = new Insets(0, 0, 5, 5);
-		gbcTxtId.gridx = 1;
-		gbcTxtId.gridy = 0;
-		getContentPane().add(txtId, gbcTxtId);
-		txtId.setColumns(10);
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 9;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		lstExamModel = new DefaultListModel<>();
+		lstExam = new JList<>(lstExamModel);
+		scrollPane.setViewportView(lstExam);
+		lstExam.setName("lstExam");
+		
+		JLabel lblErrorMessage = new JLabel(" ");
+		lblErrorMessage.setName("lblErrorMessage");
+		GridBagConstraints gbc_lblErrorMessage = new GridBagConstraints();
+		gbc_lblErrorMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblErrorMessage.gridwidth = 9;
+		gbc_lblErrorMessage.insets = new Insets(0, 0, 5, 5);
+		gbc_lblErrorMessage.gridx = 0;
+		gbc_lblErrorMessage.gridy = 1;
+		getContentPane().add(lblErrorMessage, gbc_lblErrorMessage);
 		
 		JLabel lblDescription = new JLabel("Descrizione");
 		lblDescription.setName("lblDescription");
 		GridBagConstraints gbc_lblDescription = new GridBagConstraints();
-		gbc_lblDescription.anchor = GridBagConstraints.EAST;
+		gbc_lblDescription.anchor = GridBagConstraints.WEST;
 		gbc_lblDescription.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDescription.gridx = 2;
-		gbc_lblDescription.gridy = 0;
+		gbc_lblDescription.gridx = 0;
+		gbc_lblDescription.gridy = 2;
 		getContentPane().add(lblDescription, gbc_lblDescription);
 		
 		txtDescription = new JTextField();
 		txtDescription.setName("txtDescription");
 		GridBagConstraints gbc_txtDescription = new GridBagConstraints();
-		gbc_txtDescription.gridwidth = 3;
 		gbc_txtDescription.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtDescription.insets = new Insets(0, 0, 5, 5);
-		gbc_txtDescription.gridx = 3;
-		gbc_txtDescription.gridy = 0;
+		gbc_txtDescription.gridwidth = 8;
+		gbc_txtDescription.insets = new Insets(0, 0, 5, 0);
+		gbc_txtDescription.gridx = 1;
+		gbc_txtDescription.gridy = 2;
 		getContentPane().add(txtDescription, gbc_txtDescription);
 		txtDescription.setColumns(10);
+		fieldList.add(txtDescription);
+		
+		JLabel lblId = new JLabel("Codice");
+		lblId.setName("lblId");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.insets = new Insets(0, 0, 0, 5);
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 3;
+		getContentPane().add(lblId, gbc_lblId);
+		
+		txtId = new JTextField();
+		txtId.setName("txtId");
+		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtId.insets = new Insets(0, 0, 0, 5);
+		gbc_txtId.gridx = 1;
+		gbc_txtId.gridy = 3;
+		getContentPane().add(txtId, gbc_txtId);
+		txtId.setColumns(10);
+		
+		
+		// Add all fields to fieldList
+		fieldList.add(txtId);
 		
 		JLabel lblWeight = new JLabel("Peso");
 		lblWeight.setName("lblWeight");
 		GridBagConstraints gbc_lblWeight = new GridBagConstraints();
 		gbc_lblWeight.anchor = GridBagConstraints.EAST;
 		gbc_lblWeight.insets = new Insets(0, 0, 0, 5);
-		gbc_lblWeight.gridx = 0;
-		gbc_lblWeight.gridy = 1;
+		gbc_lblWeight.gridx = 2;
+		gbc_lblWeight.gridy = 3;
 		getContentPane().add(lblWeight, gbc_lblWeight);
 		
-		txtWeight = new JTextField();
+		txtWeight = new JFormattedTextField(NumberFormat.getIntegerInstance());
 		txtWeight.setName("txtWeight");
 		GridBagConstraints gbc_txtWeight = new GridBagConstraints();
-		gbc_txtWeight.anchor = GridBagConstraints.WEST;
+		gbc_txtWeight.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtWeight.insets = new Insets(0, 0, 0, 5);
-		gbc_txtWeight.gridx = 1;
-		gbc_txtWeight.gridy = 1;
+		gbc_txtWeight.gridx = 3;
+		gbc_txtWeight.gridy = 3;
 		getContentPane().add(txtWeight, gbc_txtWeight);
 		txtWeight.setColumns(10);
+		fieldList.add(txtWeight);
 		
 		JLabel lblGrade = new JLabel("Voto");
 		lblGrade.setName("lblGrade");
 		GridBagConstraints gbc_lblGrade = new GridBagConstraints();
-		gbc_lblGrade.insets = new Insets(0, 0, 0, 5);
 		gbc_lblGrade.anchor = GridBagConstraints.EAST;
-		gbc_lblGrade.gridx = 2;
-		gbc_lblGrade.gridy = 1;
+		gbc_lblGrade.insets = new Insets(0, 0, 0, 5);
+		gbc_lblGrade.gridx = 4;
+		gbc_lblGrade.gridy = 3;
 		getContentPane().add(lblGrade, gbc_lblGrade);
 		
-		JComboBox cmbGrade = new JComboBox();
-		cmbGrade.setModel(new DefaultComboBoxModel(new String[] {"18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "30L"}));
+		cmbGrade = new JComboBox<String>();
+		cmbGrade.setName("cmbGrade");
+		cmbGrade.setModel(new DefaultComboBoxModel<String>(new String[] {"", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "30L"}));
 		GridBagConstraints gbc_cmbGrade = new GridBagConstraints();
-		gbc_cmbGrade.insets = new Insets(0, 0, 0, 5);
 		gbc_cmbGrade.fill = GridBagConstraints.HORIZONTAL;
-		gbc_cmbGrade.gridx = 3;
-		gbc_cmbGrade.gridy = 1;
+		gbc_cmbGrade.anchor = GridBagConstraints.NORTH;
+		gbc_cmbGrade.insets = new Insets(0, 0, 0, 5);
+		gbc_cmbGrade.gridx = 5;
+		gbc_cmbGrade.gridy = 3;
 		getContentPane().add(cmbGrade, gbc_cmbGrade);
 		
 		JLabel lblDate = new JLabel("Data");
@@ -108,27 +175,65 @@ public class LibrettoSwingView extends JFrame implements LibrettoView {
 		GridBagConstraints gbc_lblDate = new GridBagConstraints();
 		gbc_lblDate.anchor = GridBagConstraints.EAST;
 		gbc_lblDate.insets = new Insets(0, 0, 0, 5);
-		gbc_lblDate.gridx = 4;
-		gbc_lblDate.gridy = 1;
+		gbc_lblDate.gridx = 6;
+		gbc_lblDate.gridy = 3;
 		getContentPane().add(lblDate, gbc_lblDate);
 		
-		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-		JFormattedTextField txtDate = new JFormattedTextField(format);
+		txtDate = new JFormattedTextField(new SimpleDateFormat("dd-MM-yyyy"));
 		txtDate.setName("txtDate");
 		GridBagConstraints gbc_txtDate = new GridBagConstraints();
 		gbc_txtDate.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtDate.gridx = 5;
-		gbc_txtDate.gridy = 1;
+		gbc_txtDate.insets = new Insets(0, 0, 0, 5);
+		gbc_txtDate.gridx = 7;
+		gbc_txtDate.gridy = 3;
 		getContentPane().add(txtDate, gbc_txtDate);
-	}
+		fieldList.add(txtDate);
+		
+		btnSave = new JButton("Salva");
+		btnSave.setEnabled(false);
+		btnSave.setName("btnSave");
+		GridBagConstraints gbc_btnSave = new GridBagConstraints();
+		gbc_btnSave.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSave.anchor = GridBagConstraints.NORTH;
+		gbc_btnSave.gridx = 8;
+		gbc_btnSave.gridy = 3;
+		getContentPane().add(btnSave, gbc_btnSave);
+		
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JTextField txtId;
-	private JTextField txtDescription;
-	private JTextField txtWeight;
+		
+		// Add a document listener
+		DocumentListener listener = new DocumentListener() {
+		    @Override
+		    public void removeUpdate(DocumentEvent e) { changedUpdate(e); }
+		    @Override
+		    public void insertUpdate(DocumentEvent e) { changedUpdate(e); }
+
+		    @Override
+		    public void changedUpdate(DocumentEvent e) {
+		        boolean canEnable = true;
+		        for (JTextField tf : fieldList) {
+		            if (tf.getText().isEmpty()) {
+		                canEnable = false;
+		                break;
+		            }
+		        }
+		        
+		        canEnable = canEnable && cmbGrade.getSelectedIndex() != 0;
+		        
+		        btnSave.setEnabled(canEnable);
+		    }
+		};
+		
+		// Attach documentListener to all text fields and to combo
+		for (JTextField tf : fieldList) {
+		    tf.getDocument().addDocumentListener(listener);
+		}
+		
+	}
+	
+	DefaultListModel<Exam> getLstExamModel() {
+		return lstExamModel;
+	}
 
 	@Override
 	public void showAllExams(List<Exam> exams) {
@@ -159,5 +264,4 @@ public class LibrettoSwingView extends JFrame implements LibrettoView {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
--- src/test/java/com/example/libretto/view/swing/LibrettoSwingViewTest.java
+++ src/test/java/com/example/libretto/view/swing/LibrettoSwingViewTest.java
@@ -2,11 +2,19 @@ package com.example.libretto.view.swing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.example.libretto.model.Exam;
+import com.example.libretto.model.Grade;
+
+@RunWith(GUITestRunner.class)
 public class LibrettoSwingViewTest extends AssertJSwingJUnitTestCase {
 
 	private LibrettoSwingView librettoSwingView;
@@ -30,15 +38,75 @@ public class LibrettoSwingViewTest extends AssertJSwingJUnitTestCase {
 		assertThat(window.label("lblDescription")).isNotNull();
 		window.textBox("txtDescription").requireEnabled();
 		assertThat(window.label("lblWeight")).isNotNull();
-		window.textBox("txtdweight").requireEnabled();
+		window.textBox("txtWeight").requireEnabled();
 		assertThat(window.label("lblGrade")).isNotNull();
-		window.textBox("txtGrade").requireEnabled();
+		window.comboBox("cmbGrade").requireEnabled();
 		assertThat(window.label("lblDate")).isNotNull();
 		window.textBox("txtDate").requireEnabled();
-		window.button("btnAdd").requireDisabled();
+		window.button("btnSave").requireDisabled();
 		assertThat(window.list("lstExam")).isNotNull();
-		window.button("btnDelete").requireDisabled();
 		window.label("lblErrorMessage").requireText(" ");
+		GuiActionRunner.execute(() -> {
+			librettoSwingView.getLstExamModel().addElement(new Exam("B027500", "Data Mining and Organization", 12, new Grade("30L"), LocalDate.of(2020, 1, 29)));
+			librettoSwingView.getLstExamModel().addElement(new Exam("B027507", "Parallel Computing", 6, new Grade("27"), LocalDate.of(2020, 1, 9)));
+		});
+		window.list("lstExam").selectItem(0);
+		window.list("lstExam").selectItem(1);
+	}
+	
+	@Test
+	public void testWhenAllFieldsAreFilledSaveButtonIsEnabled() {
+		window.textBox("txtId").setText("B027507");
+		window.textBox("txtDescription").setText("Parallel Computing");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(10);
+		window.textBox("txtDate").setText("09-01-2020");
+		window.button("btnSave").requireEnabled();
+	}
+	
+	@Test
+	public void testWhenIdFieldIsEmptySaveButtonIsDisabled() {
+		window.textBox("txtDescription").setText("Parallel Computing");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(10);
+		window.textBox("txtDate").setText("09-01-2020");
+		window.button("btnSave").requireDisabled();
+	}
+	
+	@Test
+	public void testWhenDescriptionFieldIsEmptySaveButtonIsDisabled() {
+		window.textBox("txtId").setText("B027507");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(10);
+		window.textBox("txtDate").setText("09-01-2020");
+		window.button("btnSave").requireDisabled();
+	}
+	
+	@Test
+	public void testWhenWeightFieldIsEmptySaveButtonIsDisabled() {
+		window.textBox("txtId").setText("B027507");
+		window.textBox("txtDescription").setText("Parallel Computing");
+		window.comboBox("cmbGrade").selectItem(10);
+		window.textBox("txtDate").setText("09-01-2020");
+		window.button("btnSave").requireDisabled();
+	}
+	
+	@Test
+	public void testWhenGradeComboIsNotSelectedSaveButtonIsDisabled() {
+		window.textBox("txtId").setText("B027507");
+		window.textBox("txtDescription").setText("Parallel Computing");
+		window.textBox("txtWeight").setText("6");
+		window.textBox("txtDate").setText("09-01-2020");
+		window.button("btnSave").requireDisabled();
+	}
+	
+	@Test
+	public void testWhenDateFieldIsEmptySaveButtonIsDisabled() {
+		window.textBox("txtId").setText("B027507");
+		window.textBox("txtDescription").setText("Parallel Computing");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(10);
+		window.button("btnSave").requireDisabled();
 	}
 	
 
