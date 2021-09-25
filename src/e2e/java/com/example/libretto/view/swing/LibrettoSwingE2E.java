6fb85342790bed141913455b8a435f6e7baba977
MassimilianoMancini <massimiliano1.mancini@gmail.com>
2021-09-25 16:39:10 +0200

    working on e2e tests

---
+++ src/e2e/java/com/example/libretto/view/swing/LibrettoSwingE2E.java
@@ -0,0 +1,126 @@
+package com.example.libretto.view.swing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
+
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GUITestRunner.class)
+public class LibrettoSwingE2E extends AssertJSwingJUnitTestCase {
+	
+	private static final String LIBRETTO_DB_NAME = "librettotest";
+	private static Connection conn = null;
+	private static int mariadbPort = Integer.parseInt(System.getProperty("mariadb.port", "3306"));
+	private static String mariadbPassword = System.getProperty("MARIADB_ROOT_PASSWORD", "password");
+	private Statement stmt;
+	private FrameFixture window;
+	
+	@BeforeClass
+	public static void establishConnection() throws InterruptedException {
+		for (int i = 1; (conn == null) && (i < 60); i++) {
+			try {
+				conn = DriverManager.getConnection("jdbc:mariadb://localhost:" + mariadbPort, "root", mariadbPassword);
+			} catch (SQLException e) {
+				System.out.println("[ExamMariaDBRepositoryIT] DB connection attempt # " + i);
+				TimeUnit.SECONDS.sleep(1);
+			}
+		}
+	}
+	
+
+	@Override
+	protected void onSetUp() throws Exception {
+		stmt = conn.createStatement();
+		stmt.executeUpdate("drop database if exists " + LIBRETTO_DB_NAME);
+		stmt.executeUpdate("create database " + LIBRETTO_DB_NAME);
+		stmt.executeUpdate("use " + LIBRETTO_DB_NAME);
+		stmt.executeUpdate(
+				"create table libretto (" + "id varchar(7) not null primary key, " + "description varchar(60) not null,"
+						+ "weight int not null," + "grade varchar(3) not null," + "date date not null" + ")");
+		stmt.executeUpdate("insert into libretto values (" + "'B027500', " + "'Data Mining and Organization', " + "12, "
+				+ "'30L', " + "'2020-01-29')");
+
+		stmt.executeUpdate("insert into libretto values (" + "'B027507', " + "'Parallel Computing', " + "6, " + "'27', "
+				+ "'2020-01-09')");
+		application("com.example.libretto.app.swing.LibrettoSwingApp")
+			.withArgs(
+				"--host=localhost", 
+				"--port=" + mariadbPort,
+				"--user=root",
+				"--password=" + mariadbPassword,
+				"--db=" + LIBRETTO_DB_NAME)
+			.start();
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+			@Override
+			protected boolean isMatching(JFrame frame) {
+				return "Libretto universitario".equals(frame.getTitle()) && frame.isShowing();
+			}
+		}).using(robot());
+	}
+	
+	@AfterClass
+	public static void closeConnection() throws SQLException {
+		conn.close();
+	}
+	
+	@Test
+	public void testOnStartAllDatabaseElementsAreShown() {
+		assertThat(window.list().contents())
+			.anySatisfy(e -> assertThat(e).contains(
+				"B027500", 
+				"Data Mining and Organization",
+				"12",
+				"30L",
+				"2020-01-29"))
+			.anySatisfy(e -> assertThat(e).contains(
+				"B027507",
+				"Parallel Computing",
+				"6",
+				"27",
+				"2020-01-09"));
+	}
+	
+	@Test
+	public void testSaveButtonSuccess() {
+		window.textBox("txtId").setText("B027536");
+		window.textBox("txtDescription").setText("Numerical Methods for Graphics");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(9);
+		window.textBox("txtDate").setText("15-06-2020");
+		window.button("btnSave").click();
+		assertThat(window.list().contents())
+		.anySatisfy(e -> assertThat(e).contains(
+			"B027536",
+			"Numerical Methods for Graphics",
+			"6",
+			"26",
+			"2020-06-15"));
+	}
+	
+	@Test
+	public void testSaveButtonError() {
+		window.textBox("txtId").setText("B027500");
+		window.textBox("txtDescription").setText("Another fake exam");
+		window.textBox("txtWeight").setText("6");
+		window.comboBox("cmbGrade").selectItem(9);
+		window.textBox("txtDate").setText("15-06-2020");
+		window.button("btnSave").click();
+		assertThat(window.label("lblErrorMessage").text()).contains("B027500", "Data Mining and Organization");
+	}
+}
