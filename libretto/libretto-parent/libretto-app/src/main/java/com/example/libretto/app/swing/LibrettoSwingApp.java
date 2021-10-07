14a566a6614a71a9c9671daf484bcc9a1dc0d696
Massimiliano Mancini <massimiliano1.mancini@gmail.com>
2021-10-07 11:37:56 +0200

    Bump to version 1.0.4
    https://github.com/MassimilianoMancini/libretto/issues/176

--- libretto/libretto-parent/libretto-app/src/main/java/com/example/libretto/app/swing/LibrettoSwingApp.java
+++ libretto/libretto-parent/libretto-app/src/main/java/com/example/libretto/app/swing/LibrettoSwingApp.java
@@ -18,7 +18,7 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(mixinStandardHelpOptions = true, version = "1.0.3")
+@Command(mixinStandardHelpOptions = true, version = "Libretto version 1.0.4")
 public class LibrettoSwingApp implements Callable<Void> {
 	
 	private static final String SQL_ERROR_MESSAGE = "SQL Problems";
